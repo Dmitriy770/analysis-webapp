fc78370319a0fcf795956b0193f5937c3b517a7d
Dmitriy770 <zdima08@gmail.com>
2024-12-15 15:25:31 +0300

    fix

--- src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
+++ src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
@@ -6,7 +6,7 @@ namespace Uis.Infrastructure.Gateways.GitHub.Api;
 internal interface IGitHubApi
 {
     [Get("/user")]
-    [Headers("X-GitHub-Api-Version: 2022-11-2", "Accept: application/json", ."User-Agent: Uis.Api")]
+    [Headers("X-GitHub-Api-Version: 2022-11-2", "Accept: application/json", "User-Agent: Uis.Api")]
     Task<User> GetUserAsync(
         [Header("Authorization")] string authorizationHeader);
 
