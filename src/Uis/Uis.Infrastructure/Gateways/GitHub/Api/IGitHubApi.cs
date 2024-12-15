db6a0b21be87dcbfe2a8b8e843feea9b000e4b2c
Dmitriy770 <zdima08@gmail.com>
2024-12-15 15:37:00 +0300

    fix typo

--- src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
+++ src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
@@ -6,9 +6,7 @@ namespace Uis.Infrastructure.Gateways.GitHub.Api;
 internal interface IGitHubApi
 {
     [Get("/user")]
-    [Headers("X-GitHub-Api-Version: 2022-11-2", "Accept: application/json", "User-Agent: Uis.Api")]
+    [Headers("X-GitHub-Api-Version: 2022-11-28", "Accept: application/json", "User-Agent: Uis.Api")]
     Task<User> GetUserAsync(
         [Header("Authorization")] string authorizationHeader);
-
-    private const string ApiVersion = "2022-11-28";
 }
