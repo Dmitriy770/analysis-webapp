422abf9a41f0cefb8f2be75e93f9a6cf12174d10
Dmitriy770 <zdima08@gmail.com>
2024-12-15 15:24:53 +0300

    add user agent

--- src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubOAuthApi.cs
+++ src/Uis/Uis.Infrastructure/Gateways/GitHub/Api/IGitHubOAuthApi.cs
@@ -6,7 +6,7 @@ namespace Uis.Infrastructure.Gateways.GitHub.Api;
 internal interface IGitHubOAuthApi
 {
     [Get("/login/oauth/access_token")]
-    [Headers("X-GitHub-Api-Version: 2022-11-2", "Accept: application/json")]
+    [Headers("X-GitHub-Api-Version: 2022-11-2", "Accept: application/json", "User-Agent: Uis.Api")]
     Task<AccessTokenInfo> GetAccessTokenAsync(
         [Query]GetAccessTokenParams accessTokenParams);
 }
