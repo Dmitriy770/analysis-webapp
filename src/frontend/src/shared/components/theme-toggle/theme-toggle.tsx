dc6dd811022497ebe49b9c418d0ebd39126f4e47
Picalines <picalines0@gmail.com>
2024-12-17 13:48:44 +0300

    feat(frontend): landing

--- src/frontend/src/shared/components/theme-toggle/theme-toggle.tsx
+++ src/frontend/src/shared/components/theme-toggle/theme-toggle.tsx
@@ -16,8 +16,8 @@ export const ThemeToggle: FC<Props> = ({ className, ...buttonProps }) => {
   }, [setTheme])
 
   return (
-    <Button {...buttonProps} className={cn('space-x-2', className)} onClick={toggleTheme}>
-      <span className="relative h-[24px] w-[24px]">
+    <Button shape="square" {...buttonProps} className={cn('relative space-x-2', className)} onClick={toggleTheme}>
+      <span className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 h-[24px] w-[24px]">
         <SunIcon className="absolute rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
         <MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       </span>
