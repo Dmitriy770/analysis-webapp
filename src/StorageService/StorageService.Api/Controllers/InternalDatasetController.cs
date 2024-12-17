ce2d9451a3569f84274794db2d485bb9ba2733db
Dmitriy770 <zdima08@gmail.com>
2024-12-18 02:40:03 +0300

    fix

--- src/StorageService/StorageService.Api/Controllers/InternalDatasetController.cs
+++ src/StorageService/StorageService.Api/Controllers/InternalDatasetController.cs
@@ -16,43 +16,43 @@ public sealed class InternalDatasetController(
     ISender sender)
     : ControllerBase
 {
-    // [HttpGet("{id:guid}")]
-    // [ProducesResponseType<FileContentHttpResult>(StatusCodes.Status200OK, ContentType)]
-    // [ProducesResponseType<ErrorResponse>(StatusCodes.Status404NotFound)]
-    // public async Task<IResult> GetById(
-    //     Guid id,
-    //     CancellationToken cancellationToken = default)
-    // {
-    //     var result = await sender.Send(new GetContentByIdQuery(id), cancellationToken);
-    //
-    //     var content = await StreamToBytes(result.Content, cancellationToken);
-    //     return Results.File(content, ContentType, result.Name);
-    // }
-    //
-    // [HttpGet("{id:guid}/description")]
-    // [ProducesResponseType<DatasetDescription>(StatusCodes.Status200OK)]
-    // [ProducesResponseType<ErrorResponse>(StatusCodes.Status404NotFound)]
-    // public async Task<IResult> GetDescription(
-    //     Guid id,
-    //     CancellationToken cancellationToken = default)
-    // {
-    //     var description = await sender.Send(new GetDescriptionByIdQuery(id), cancellationToken);
-    //     
-    //     return Results.Ok(description.ToApi());
-    // }
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType<FileContentHttpResult>(StatusCodes.Status200OK, ContentType)]
+    [ProducesResponseType<ErrorResponse>(StatusCodes.Status404NotFound)]
+    public async Task<IResult> GetById(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await sender.Send(new GetContentByIdQuery(id), cancellationToken);
     
-    // [HttpGet("/description")]
-    // [ProducesResponseType<DatasetsDescription>(StatusCodes.Status200OK)]
-    // public async Task<IResult> GetDescriptionsByUserId(
-    //     [FromQuery(Name = "userId")] long userId,
-    //     CancellationToken cancellationToken = default)
-    // {
-    //     var descriptions = sender.CreateStream(new GetDescriptionsByUserIdQuery(userId), cancellationToken);
-    //
-    //     return Results.Ok(await descriptions.ToApi(cancellationToken));
-    // }
-    //
-    [HttpGet("/description")]
+        var content = await StreamToBytes(result.Content, cancellationToken);
+        return Results.File(content, ContentType, result.Name);
+    }
+    
+    [HttpGet("{id:guid}/description")]
+    [ProducesResponseType<DatasetDescription>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ErrorResponse>(StatusCodes.Status404NotFound)]
+    public async Task<IResult> GetDescription(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        var description = await sender.Send(new GetDescriptionByIdQuery(id), cancellationToken);
+        
+        return Results.Ok(description.ToApi());
+    }
+    
+    [HttpGet("description")]
+    [ProducesResponseType<DatasetsDescription>(StatusCodes.Status200OK)]
+    public async Task<IResult> GetDescriptionsByUserId(
+        [FromQuery(Name = "userId")] long userId,
+        CancellationToken cancellationToken = default)
+    {
+        var descriptions = sender.CreateStream(new GetDescriptionsByUserIdQuery(userId), cancellationToken);
+    
+        return Results.Ok(await descriptions.ToApi(cancellationToken));
+    }
+    
+    [HttpGet("description")]
     [ProducesResponseType<DatasetsDescription>(StatusCodes.Status200OK)]
     public async Task<IResult> GetDescriptionByUserIdAndName(
         [FromQuery(Name = "userId")] long userId,
