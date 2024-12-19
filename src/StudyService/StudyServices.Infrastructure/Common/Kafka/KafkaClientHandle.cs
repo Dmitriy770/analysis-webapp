90a0abbfa39a318b8bc77a108fa5ca7e07b3d937
Dmitriy770 <zdima08@gmail.com>
2024-12-19 21:42:55 +0300

    update password

--- src/StudyService/StudyServices.Infrastructure/Common/Kafka/KafkaClientHandle.cs
+++ src/StudyService/StudyServices.Infrastructure/Common/Kafka/KafkaClientHandle.cs
@@ -15,7 +15,7 @@ internal sealed class KafkaClientHandle : IDisposable
             SecurityProtocol = SecurityProtocol.SaslPlaintext,
             SaslMechanism = SaslMechanism.ScramSha512,
             SaslUsername = "gen_user",
-            SaslPassword = "SnIXh2p6DfP9g-",
+            SaslPassword = "SnlXh2p6DfP9gs.",
             AllowAutoCreateTopics = false,
             ClientId = "StudyServices",
             SslEndpointIdentificationAlgorithm = SslEndpointIdentificationAlgorithm.None
