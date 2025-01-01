09c8beefb6edbae04fa3050092940e4f2eb56a4a
Dmitriy770 <zdima08@gmail.com>
2025-01-01 19:02:17 +0300

    update to study service app deployment

--- src/StudyService/StudyService.Automation/deploy.sh
+++ src/StudyService/StudyService.Automation/deploy.sh
@@ -7,5 +7,6 @@ helm upgrade studies-service oci://registry-1.docker.io/bitnamicharts/aspnet-cor
     --namespace business-logic-services \
     --timeout 5m0s \
     --values values.yaml \
+    --set appFromExternalRepo.clone.revision="$BRANCH" \
     --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_USER" \
     --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_PASSWORD" \
