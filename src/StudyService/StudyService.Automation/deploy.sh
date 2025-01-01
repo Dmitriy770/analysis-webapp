2688ca99b435b6d3e936fc3e73d819579794a9b2
Dmitriy770 <zdima08@gmail.com>
2025-01-01 18:14:00 +0300

    test deploy

--- src/StudyService/StudyService.Automation/deploy.sh
+++ src/StudyService/StudyService.Automation/deploy.sh
@@ -5,7 +5,7 @@ helm upgrade studies-service oci://registry-1.docker.io/bitnamicharts/aspnet-cor
     --atomic \
     --create-namespace \
     --namespace business-logic-services \
-    --timeout 2m0s \
+    --timeout 5m0s \
     --values values.yaml \
     --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_USER" \
     --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_PASSWORD" \
