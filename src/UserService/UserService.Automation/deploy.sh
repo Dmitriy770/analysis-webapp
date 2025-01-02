597b5fa0c29424dcf60e27c8aba501f5f8a3e3cd
Dmitriy770 <zdima08@gmail.com>
2025-01-02 21:37:39 +0300

    update deployment for UserService

---
+++ src/UserService/UserService.Automation/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+helm upgrade user-service oci://registry-1.docker.io/bitnamicharts/aspnet-core \
+    --install \
+    --atomic \
+    --create-namespace \
+    --namespace business-logic-services \
+    --timeout 2m0s \
+    --values values.yaml \
+    --set appFromExternalRepo.clone.revision="$BRANCH" \
+    --set kong.extraEnvVars[0].value="$SESSION_STORAGE_USER" \
+    --set kong.extraEnvVars[1].value="$SESSION_STORAGE_PASSWORD" \
+    --set kong.extraEnvVars[2].value="$USER_STORAGE_USER" \
+    --set kong.extraEnvVars[3].value="$USER_STORAGE_PASSWORD" \
+    --set kong.extraEnvVars[4].value="$GITHUB_CLIENT_ID" \
+    --set kong.extraEnvVars[5].value="$GITHUB_SECRET"
