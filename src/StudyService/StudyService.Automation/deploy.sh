e6ce90710690df5ddcf228e6f448b0e78813d48e
Dmitriy770 <zdima08@gmail.com>
2025-01-02 20:40:41 +0300

    update limits

--- src/StudyService/StudyService.Automation/deploy.sh
+++ src/StudyService/StudyService.Automation/deploy.sh
@@ -5,7 +5,7 @@ helm upgrade studies-service oci://registry-1.docker.io/bitnamicharts/aspnet-cor
     --atomic \
     --create-namespace \
     --namespace business-logic-services \
-    --timeout 5m0s \
+    --timeout 2m0s \
     --values values.yaml \
     --set appFromExternalRepo.clone.revision="$BRANCH" \
     --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_USER" \
