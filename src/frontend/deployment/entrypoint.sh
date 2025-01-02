656737a5d6cbe826ff39fc3eabeb77df4cde3e85
Dmitriy770 <zdima08@gmail.com>
2025-01-02 15:20:14 +0300

    update frontend deployment

--- src/frontend/deployment/entrypoint.sh
+++ src/frontend/deployment/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+getent group developers 
+
 export PNPM_HOME="/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
 cd ./src/frontend || exit
 
-ls ../lib/node_modules/corepack/
+ls ../lib/node_modules
 
 corepack enable
+
+ls ../lib/node_modules/corepack/
 
 pnpm --version
 
