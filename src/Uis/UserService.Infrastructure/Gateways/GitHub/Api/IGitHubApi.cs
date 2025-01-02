881a11d14c092ba57004f8b134d5d6249d4808a2
Dmitriy770 <zdima08@gmail.com>
2025-01-02 21:19:29 +0300

    rename UIS to UserService

--- src/Uis/UserService.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
+++ src/Uis/UserService.Infrastructure/Gateways/GitHub/Api/IGitHubApi.cs
@@ -6,7 +6,7 @@ namespace Uis.Infrastructure.Gateways.GitHub.Api;
 internal interface IGitHubApi
 {
     [Get("/user")]
-    [Headers("X-GitHub-Api-Version: 2022-11-28", "Accept: application/json", "User-Agent: Uis.Api")]
+    [Headers("X-GitHub-Api-Version: 2022-11-28", "Accept: application/json", "User-Agent: UserServiceUserService.Api")]
     Task<User> GetUserAsync(
         [Header("Authorization")] string authorizationHeader);
 }
--- src/Uis/UserService.Infrastructure/Gateways/GitHub/Api/IGitHubOAuthApi.cs
+++ src/Uis/UserService.Infrastructure/Gateways/GitHub/Api/IGitHubOAuthApi.cs
@@ -6,7 +6,7 @@ namespace Uis.Infrastructure.Gateways.GitHub.Api;
 internal interface IGitHubOAuthApi
 {
     [Get("/login/oauth/access_token")]
-    [Headers("X-GitHub-Api-Version: 2022-11-28", "Accept: application/json", "User-Agent: Uis.Api")]
+    [Headers("X-GitHub-Api-Version: 2022-11-28", "Accept: application/json", "User-Agent: UserService.Api")]
     Task<AccessTokenInfo> GetAccessTokenAsync(
         [Query]GetAccessTokenParams accessTokenParams);
 }
