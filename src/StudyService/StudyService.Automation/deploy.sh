391e78e14fe0202d00a981207c0f80babb11c055
Dmitriy770 <zdima08@gmail.com>
2025-01-02 21:54:23 +0300

    update env variables

--- src/StudyService/StudyService.Automation/deploy.sh
+++ src/StudyService/StudyService.Automation/deploy.sh
@@ -8,5 +8,5 @@ helm upgrade studies-service oci://registry-1.docker.io/bitnamicharts/aspnet-cor
     --timeout 2m0s \
     --values values.yaml \
     --set appFromExternalRepo.clone.revision="$BRANCH" \
-    --set kong.extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_USER" \
-    --set kong.extraEnvVars[1].value="$KAFKA_STUDY_SERVICE_BROKER_PASSWORD" \
+    --set extraEnvVars[0].value="$KAFKA_STUDY_SERVICE_BROKER_USER" \
+    --set extraEnvVars[1].value="$KAFKA_STUDY_SERVICE_BROKER_PASSWORD" \
--- src/UserService/UserService.Automation/deploy.sh
+++ src/UserService/UserService.Automation/deploy.sh
@@ -8,9 +8,9 @@ helm upgrade user-service oci://registry-1.docker.io/bitnamicharts/aspnet-core \
     --timeout 4m0s \
     --values values.yaml \
     --set appFromExternalRepo.clone.revision="$BRANCH" \
-    --set kong.extraEnvVars[0].value="$SESSION_STORAGE_USER" \
-    --set kong.extraEnvVars[1].value="$SESSION_STORAGE_PASSWORD" \
-    --set kong.extraEnvVars[2].value="$USER_STORAGE_USER" \
-    --set kong.extraEnvVars[3].value="$USER_STORAGE_PASSWORD" \
-    --set kong.extraEnvVars[4].value="$GITHUB_CLIENT_ID" \
-    --set kong.extraEnvVars[5].value="$GITHUB_SECRET"
+    --set extraEnvVars[0].value="$SESSION_STORAGE_USER" \
+    --set extraEnvVars[1].value="$SESSION_STORAGE_PASSWORD" \
+    --set extraEnvVars[2].value="$USER_STORAGE_USER" \
+    --set extraEnvVars[3].value="$USER_STORAGE_PASSWORD" \
+    --set extraEnvVars[4].value="$GITHUB_CLIENT_ID" \
+    --set extraEnvVars[5].value="$GITHUB_SECRET"
