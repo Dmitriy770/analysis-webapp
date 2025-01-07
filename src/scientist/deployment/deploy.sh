245ec03920af0664d6df64fde6b4016f69f92605
Dmitriy770 <zdima08@gmail.com>
2025-01-07 17:47:31 +0300

    add debug logs

--- src/scientist/deployment/deploy.sh
+++ src/scientist/deployment/deploy.sh
@@ -2,12 +2,20 @@
 
 export KAFKA_SERVER='kafka.common-infra.svc.cluster.local:9092'
 
+echo "test 0"
+
 sed -i "s\{template-scientist-image}\ghcr.io/dmitriy770/analysis-webapp-scientist:$IMAGE_TAG\g" scientist-deployment.yaml
 
 sed -i "s\{template-kafka-server}\'$KAFKA_SERVER'\g" scientist-deployment.yaml
 sed -i "s\{template-kafka-user}\'$KAFKA_USER'\g" scientist-deployment.yaml
 sed -i "s\{template-kafka-password}\'$KAFKA_PASSWORD'\g" scientist-deployment.yaml
 
+echo "test 1"
+
 kubectl apply -f scientist-deployment.yaml
 
+echo "test 2"
+
 kubectl apply -f scientist-service.yaml
+
+echo "test 3"
