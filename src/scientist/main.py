93442371f4e4874a7b20c8b48a3a23752b41440b
Dmitriy770 <zdima08@gmail.com>
2025-01-07 21:42:08 +0300

    add debug logs

--- src/scientist/main.py
+++ src/scientist/main.py
@@ -40,6 +40,7 @@ def main():
         dataset_contents = get_dataset_contents(dataset_id=dataset_id)
         points = clusterize_dataset(dataset_contents=dataset_contents, columns=dataset_columns)
 
+        print(list(map(lambda p: p.to_json(), points))
         post_study_result(study_id=study_id, points=points)
 
     consumer.close()
