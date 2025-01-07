af1364a12c8ab6c24ef1b7fd7a1fe11e479ba1ee
Dmitriy770 <zdima08@gmail.com>
2025-01-08 00:20:08 +0300

    update scientist

--- src/scientist/app/api/study.py
+++ src/scientist/app/api/study.py
@@ -6,4 +6,4 @@ study_client = httpx.Client(base_url="http://studies-service-aspnet-core.busines
 
 
 def post_study_result(*, study_id: str, points: list[Point]):
-    study_client.post(f"/internal/studies/{study_id}/result", json={"points": list(map(lambda p: p.to_json(), points))})
+    study_client.post(f"/internal/studies/{study_id}/result", data={"points": list(map(lambda p: p.to_json(), points))})
