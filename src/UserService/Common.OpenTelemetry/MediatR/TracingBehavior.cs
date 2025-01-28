a0367176705470e2865e4a092d823911b04726be
Dmitriy770 <zdima08@gmail.com>
2025-01-28 23:15:49 +0300

    add OpenTelemtry Common

---
+++ src/UserService/Common.OpenTelemetry/MediatR/TracingBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using MediatR;
+
+namespace Common.OpenTelemetry.MediatR;
+
+public sealed class TracingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private Activity? StartActiveSpan()
+    {
+        Consumer
+        var activity = Tracing.WebActivitySource.StartActivity(ProducingActivity, ActivityKind.Producer);
+        if (activity is not null)
+        {
+            var propagationContext = new PropagationContext(activity.Context, Baggage.Current);
+            Propagators.DefaultTextMapPropagator.Inject(propagationContext, message.Headers ??= new Headers(),
+                (headers, key, value) => headers.Add(key, Encoding.UTF8.GetBytes(value)));
+        }
+        return activity;
+    }
+    
+    private static readonly AssemblyName CurrentAssembly = typeof(TracingBehavior<,>).Assembly.GetName();
+    private static string Version => CurrentAssembly.Version!.ToString();
+    private static string AssemblyName => CurrentAssembly.Name!;
+    public static readonly ActivitySource ConsumerActivitySource = new(AssemblyName, Version);
+}
